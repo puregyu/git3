8d00ee1ea5d96f3a3382b679c1c8b88ed95a7166
황상진 <sangjin5579@naver.com>
2020-04-25 13:22:55 +0900

    dev_sangjin2 브런치 추가

--- Jongro3/src/Test.java
+++ Jongro3/src/Test.java
@@ -3,5 +3,6 @@ public class Test {
         System.out.println("테스트1");
         System.out.println("dev 브랜치 추가");
         System.out.println("dev_sangjin 브랜치 추가");
+        System.out.println("dev_sangjin2 브랜치 추가ㅎ");
     }
 }
